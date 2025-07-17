19c053155e6858200ffba8b9d67863f1d23b8813
CoRine65 <coderine65@gmail.com>
2025-07-17 07:19:43 -0700

    -set validations for User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 8 }
 end
